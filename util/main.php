d50e59359c5334b38dca619b8ab46c185681b5d3
Ross3102 <rosnew20@bergen.org>
2018-04-27 14:46:37 -0400

    heroku

--- util/main.php
+++ util/main.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__FILE__) . "/../../config.php";
+//require_once dirname(__FILE__) . "/../../config.php";
 include dirname(__FILE__) . "/../model/userdb.php";
 require_once dirname(__FILE__) . "/../vendor/autoload.php";
 
