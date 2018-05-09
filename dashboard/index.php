2eec69c0fb2a627c9abf0ae3071d3619f643dc98
Ross3102 <rosnew20@bergen.org>
2018-05-08 20:27:44 -0400

    clarified purpose of schedule page

--- dashboard/index.php
+++ dashboard/index.php
@@ -30,11 +30,13 @@ writeHeader($head);
     <h3 class="title">Action Dashboard</h3>
     <div class="collection">
         <?php
-            create_dashboard_item("schedules", "Manage Schedules",
-                "Run or edit existing schedules or create a new one!", "view_list");
-
             create_dashboard_item("work", "Task List",
                 "Write down upcoming tasks or assignments", "view_agenda");
+            create_dashboard_item("schedules", "Manage Schedules",
+                "Use this tool to manage your time effectively. Set aside time to complete your work and " .
+                "establish deadlines to motivate you not to procrastinate! Don't worry if you run out of time, " .
+                "because the deadlines can be extended when time runs out, but try to set reasonable goals and " .
+                "strive to complete them!", "view_list");
 /*  TODO: Add the following:
  *            create_dashboard_item("notes", "Notes",
  *                "Create and view small reminders and other notes", "edit");
