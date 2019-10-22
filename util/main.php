624ee6a1695b4bbc7f047dec35071777ce5eb9e1
Ross3102 <rosnew20@bergen.org>
2019-10-22 09:46:48 -0400

    Meta tags

--- util/main.php
+++ util/main.php
@@ -79,6 +79,9 @@ function writeHeader($currentPage, $head='') {
     echo '
     <html>
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>BCA Task Manager</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
         <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
