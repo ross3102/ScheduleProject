94e880bb278528f7cd74977611ff69bdd6de2ef7
Ross Newman <rzn3102@gmail.com>
2019-11-18 16:35:59 -0500

    Calendar dynamic text color

--- calendar/view.php
+++ calendar/view.php
@@ -125,7 +125,7 @@ $numCategories = count($categories);
                                         echo "<td><div class='day'>" . $cur_date;
                                 }
                                 ?>
-                                <div class="task" style="color: white; background-color: <?php echo $category_color ?>"><?php echo $task_name ?></div>
+                                <div class="task" style="background-color: <?php echo $category_color ?>"><?php echo $task_name ?></div>
                             <?php } endforeach;
 
                             while ($cur_date < $month_lengths[$month_num - 1]) {
@@ -157,7 +157,28 @@ $numCategories = count($categories);
     $(document).ready(function() {
         $("select").val(<?php echo $month_num ?>);
         $("select").formSelect();
+        $(".task").forEach(function(task) {
+            var bgColor = task.css("background-color");
+            var nThreshold = 105;
+            var components = getRGBComponents(bgColor);
+            var bgDelta = (components.R * 0.299) + (components.G * 0.587) + (components.B * 0.114);
+
+            task.css("color", ((255 - bgDelta) < nThreshold) ? "#000000" : "#ffffff");
+        });
     });
+
+    function getRGBComponents(color) {
+
+        var r = color.substring(1, 3);
+        var g = color.substring(3, 5);
+        var b = color.substring(5, 7);
+
+        return {
+            R: parseInt(r, 16),
+            G: parseInt(g, 16),
+            B: parseInt(b, 16)
+        };
+    }
 
     $("#month").change(function() {
         $("form").submit();
